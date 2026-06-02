ec6117f7ec2fb53d4e20e89b6a2425e17b5cf0f3
hoshinetsu <git@hoshinetsu.moe>
2026-06-02 17:12:08 +0200

    UPDATE Perms Schema

--- database/08_roles_permissions.sql
+++ database/08_roles_permissions.sql
@@ -1,14 +1,14 @@
 -- Tworzenie użytkownika dla aplikacji
-CREATE USER app_user WITH PASSWORD 'app_password';
+CREATE USER ApplicationIdentity WITH PASSWORD 'gGaaFEeAZg9nNfQk';
 
 -- Nadanie uprawnień do bazy (zakładając nazwę bazy smuggling_db)
-GRANT CONNECT ON DATABASE smuggling_db TO app_user;
+GRANT CONNECT ON DATABASE projekt TO ApplicationIdentity;
 
 -- Uprawnienia w schemacie public
-GRANT USAGE, CREATE ON SCHEMA public TO app_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO app_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO app_user;
+GRANT USAGE, CREATE ON SCHEMA public TO ApplicationIdentity;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ApplicationIdentity;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ApplicationIdentity;
 
 -- Automatyczne nadawanie uprawnień do nowych tabel i sekwencji utworzonych w przyszłości
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO app_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO app_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO ApplicationIdentity;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO ApplicationIdentity;
