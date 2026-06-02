39b1f8f5d5f4771608aa84b26a71190084ca32a2
hoshinetsu <git@hoshinetsu.moe>
2026-06-02 18:35:17 +0200

    SET ready for Tomcat deployment

--- backend/src/main/java/pl/edu/pb/smuggling/BackendApplication.java
+++ backend/src/main/java/pl/edu/pb/smuggling/BackendApplication.java
@@ -3,6 +3,8 @@ package pl.edu.pb.smuggling;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.edu.pb.smuggling.user.model.User;
@@ -10,7 +12,12 @@ import pl.edu.pb.smuggling.user.repository.RoleRepository;
 import pl.edu.pb.smuggling.user.repository.UserRepository;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
