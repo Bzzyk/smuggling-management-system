56cb2978663cec65c73765cafc74cb4a5b466729
hoshinetsu <62859523+hoshinetsu@users.noreply.github.com>
2026-06-16 11:40:43 +0200

    FIX SQL perms

--- database/08_roles_permissions.sql
+++ database/08_roles_permissions.sql
@@ -1,14 +1,22 @@
 -- Tworzenie użytkownika dla aplikacji
 CREATE USER ApplicationIdentity WITH PASSWORD 'gGaaFEeAZg9nNfQk';
 
--- Nadanie uprawnień do bazy (zakładając nazwę bazy smuggling_db)
 GRANT CONNECT ON DATABASE projekt TO ApplicationIdentity;
+GRANT USAGE ON SCHEMA public TO ApplicationIdentity;
 
--- Uprawnienia w schemacie public
-GRANT USAGE, CREATE ON SCHEMA public TO ApplicationIdentity;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ApplicationIdentity;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ApplicationIdentity;
+-- CRUD
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON ALL TABLES IN SCHEMA public
+TO ApplicationIdentity;
 
--- Automatyczne nadawanie uprawnień do nowych tabel i sekwencji utworzonych w przyszłości
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO ApplicationIdentity;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO ApplicationIdentity;
+GRANT USAGE, SELECT, UPDATE
+ON ALL SEQUENCES IN SCHEMA public
+TO ApplicationIdentity;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON TABLES TO ApplicationIdentity;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT USAGE, SELECT, UPDATE
+ON SEQUENCES TO ApplicationIdentity;
